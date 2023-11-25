ddc6b03261839d606494ffb154667f34a4845d28
wanjala stephen <stephenwanjala145@gmail.com>
2023-11-25 23:10:27 +0300

    Refactor projectStore.ts: Improve Pinia store structure and Use Vue 3 composition API Style

--- src/store/projectStore.ts
+++ src/store/projectStore.ts
@@ -1,23 +1,25 @@
-import { defineStore } from 'pinia';
-import {  getProjectWithStars } from '@/projects/viewmodel';
-import type { Project } from '@/projects/viewmodel';
+import {defineStore} from 'pinia';
+import type {Project} from '@/projects/viewmodel';
+import {getProjectWithStars} from '@/projects/viewmodel';
+import {ref} from "vue";
 
-export const useProjectStore = defineStore('project', {
-  state: () => ({
-    projects: [] as Project[],
-  }),
-  actions: {
-    async fetchProjects() {
-      try {
-        const projects = await new Promise<Project[]>((resolve) => {
-          getProjectWithStars((result: Project[]) => {
-            resolve(result);
-          });
-        });
-        this.projects = projects;
-      } catch (error) {
-        console.error(error);
-      }
-    },
-  },
+export const useProjectStore = defineStore('project', () => {
+    const projects = ref([] as Project[]);
+
+    async function fetchProjects() {
+        try {
+          projects.value = await new Promise<Project[]>((resolve) => {
+              getProjectWithStars((result: Project[]) => {
+                resolve(result);
+              });
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    return {
+        projects,
+        fetchProjects
+    };
 });
