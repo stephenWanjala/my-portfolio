126ea5f4a2db333ba0f5f2e551c2b377bace36bc
Wnajala Stephen <stephenwanjala145@gmail.com>
2024-09-20 16:12:57 +0300

    Experiences Section UpDate

--- src/projects/viewmodel.ts
+++ src/projects/viewmodel.ts
@@ -1,4 +1,4 @@
-import type { Education, Experience, Language, Skill } from "@/types/Types";
+import type {Education, Experience, Language, Skill} from "@/types/Types";
 import axios from "axios";
 import type {AxiosResponse} from "axios";
 
@@ -23,7 +23,7 @@ export interface Project {
     contributors?: Contributor[]
 }
 
-export let projects: Project[] = [
+export let projects: Project[] = Array.of<Project>(
     {
         name: "geoSpartial_village",
         image: "geosparcial.jpeg",
@@ -75,17 +75,16 @@ export let projects: Project[] = [
         tags: ["Go", "gorm", "mysql"]
     },
     {
-        name :"RevVault",
-        image:"bookstore.png",
-        url:"https://github.com/stephenWanjala/RevVault.git",
-        gitName:"stephenWanjala/RevVault",
+        name: "RevVault",
+        image: "bookstore.png",
+        url: "https://github.com/stephenWanjala/RevVault.git",
+        gitName: "stephenWanjala/RevVault",
         stars: "?",
         forks: "?",
-        description:"Share && Access a comprehensive collection of past papers, study guides, and practice questions to prepare for your exams and achieve academic success.",
-        tags:["Kotlin","ktor","PostgreSQL","Exposed Framework"]
+        description: "Share && Access a comprehensive collection of past papers, study guides, and practice questions to prepare for your exams and achieve academic success.",
+        tags: ["Kotlin", "ktor", "PostgreSQL", "Exposed Framework"]
     }
-
-]
+)
 
 export interface projectWithStarsCallBack {
     (result: Project[]): void
@@ -98,7 +97,7 @@ export function getProjectWithStars(onFinish: projectWithStarsCallBack) {
             let response1 = await axios.get(`https://api.github.com/repos/${project.gitName}`);
             project.stars = response1.data.stargazers_count;
             project.forks = response1.data.forks;
-            let response2: AxiosResponse<any,any> = await axios.get(`https://api.github.com/repos/${project.gitName}/contributors`);
+            let response2: AxiosResponse<any, any> = await axios.get(`https://api.github.com/repos/${project.gitName}/contributors`);
             project.contributors = response2.data.map((contributor: Contributor) => contributor);
             newProjects.push(project);
             console.log(newProjects);
@@ -123,108 +122,130 @@ export function getProjectWithStars(onFinish: projectWithStarsCallBack) {
 }
 
 export function getImageUrl(path: string): string {
-    return new URL(`/src/assets/images/${path}`, import.meta.url).href;
+    let url = new URL(`/src/assets/images/${path}`, import.meta.url)
+    return url.href;
 }
 
 export function openLink(url: string) {
     window.open(url, "_blank");
 }
 
-export const experiences: Experience[] = [
+export const experiences: Experience[] = Array.of<Experience>(
     {
-      jobTitle: "Software Developer Intern",
-      time: "May 2024 - Present",
-      company:"Primesoft Solutions Limited",
-      companyLink: "https://primesoft.co.ke",
-      details: [
-        "Contributing to development and maintainance of software applications, including mobile applications, and desktop applications under the MaliPlus ERP.",
-        "Collaborating with cross-functional teams to analyze, design, and implement assigned new features,modules and enhancements For MaliPlus ERP .",
-        "Writing clean, maintainable, and efficient code, adhering to best practices and coding standards.",
-        "Debugging, and troubleshooting software issues to ensure optimal performance and reliability.",
-        "Providing technical support to clients, addressing their inquiries, troubleshooting issues, and offering guidance on using software applications effectively.",
-        "Contributing to the development of software architecture, design patterns, and coding standards to ensure consistency and scalability.",
-        "Participating in team meetings, stand-ups, and sprint planning sessions to coordinate work and prioritize tasks effectively."
-      ],
-    },
-    {
-      jobTitle: "Information Technology Attachee",
-      time: "May 2023 - July 2023",
-      company: "Kibabii University",
-      companyLink: "http://www.kibu.ac.ke",
-      details: [
-        "Configuring managed switches, implementing VLANs, and securing network communications through SSH and Telnet protocols.",
-        "Performing general computer maintenance tasks, including hardware upgrades, memory enhancement, and installation of operating systems and software applications.",
-        "Installing and configuring ABUNO ERP software, ensuring seamless operation and maintaining compliance with updates and patches.",
-        "Designing and executing scripts for database automation, data normalization, and denormalization, streamlining data management processes.",
-        "Managing and configuring IP cameras, editing captured videos for presentations, and maintaining the security and integrity of video evidence.",
-        "Providing technical support to students and staff, troubleshooting software-related issues, and offering guidance on using software applications effectively.",
-        "Collaborating with cross-functional teams to enhance IT infrastructure, including transitioning from unmanaged to managed switches and optimizing access points and point-to-point radios.",
-        "Assisting in the training of staff to effectively use the ERP software and troubleshoot any issues they encountered.",
-        "Developing software solutions for clients, addressing their specific needs and contributing to the department's services."
-      ],
-    },
-  ];
+        company: "Primesoft Solutions Limited",
+        companyLink: "https://primesoft.co.ke",
+        logo: "primesoft.png",
+        roles: [
+            {
+                time: {start: new Date("2024-09-01"), current: true},
+                jobTitle: "Software Developer",
+                details: [
+                    "Developing and maintaining software applications, including mobile, and desktop applications under the MaliPlus ERP.",
+                    "Design, and implement assigned features, modules, and enhancements for MaliPlus ERP.",
+                    "Collaborate with Support Team to troubleshoot and resolve software issues.",
 
-  export  const skills: Skill[] = [
-    {
-      title: "Django & Rest Framework",
-      level: 100,
+                ]
+            },
+            {
+                time: {start: new Date("2024-05-25"), end: new Date("2024-08-30")},
+                jobTitle: "Software Developer Intern",
+                details: [
+                    "Contributing to development and maintenance of software desktop applications under the MaliPlus ERP.",
+                    "Collaborating with cross-functional teams to analyze, design, and implement assigned new features, modules and enhancements for MaliPlus ERP.",
+                    "Writing clean, maintainable, and efficient code, adhering to best practices and coding standards.",
+                    "Debugging, and troubleshooting software issues ",
+                    "Providing technical support to clients, addressing their inquiries, troubleshooting issues, and offering guidance on using software applications effectively.",
+                    "Contributing to the development of software architecture, design patterns, and coding standards to ensure consistency and scalability.",
+                    "Participating in team meetings, stand-ups, and sprint planning sessions to coordinate work and prioritize tasks effectively."
+                ]
+            }
+        ]
     },
     {
-      title: "Kotlin & Jetpack Compose",
-      level: 100,
-    },
-    {
-      title: "Java & Java Fx",
-      level: 100,
-    },
-    {
-      title: "Kotlin Ktor & Spring Boot",
-      level: 100,
-    },
-  
-    {
-      title: "Relational Databases",
-      level: 100,
-    },
-    {
-      title: "HTML5 & CSS",
-      level: 100,
-    },
-    {
-      title: "Vue Js",
-      level: 100,
-    },
-    {
-      title: "React ,Next Js && Express",
-      level: 100,
-    },
-    {
-      title: "Golang",
-      level: 60,
-    },
-    {
-      title: "Sketch & Adobe Express",
-      level: 100,
-    },
-  ];
-  
-  
-  export const educations: Education[] = [
-    {
-      degree: "Bsc. Information Technology",
-      school: "Maseno University",
-      duration: "2020 - May 2024",
-    },
-    {
-      degree: "Kenya Certificate of Secondary Education",
-      school: "St Peter's Sang'alo High School",
-      duration: "2015 - 2019",
-    },
-  ];
+        company: "Kibabii University",
+        companyLink: "https://kibu.ac.ke",
+        logo: "kibabii.png",
+        roles: [
+            {
+                time: {start: new Date("2023-05-25"), end: new Date("2023-08-08")},
+                jobTitle: "Information Technology Attachment",
+                details: [
+                    "Configuring managed switches, implementing VLANs, and securing network communications through SSH and Telnet protocols.",
+                    "Performing general computer maintenance tasks, including hardware upgrades, memory enhancement, and installation of operating systems and software applications.",
+                    "Installing and configuring ABNO ERP software, ensuring seamless operation and maintaining compliance with updates and patches.",
+                    "Designing and executing scripts for database automation, data normalization, and de-normalization, streamlining data management processes.",
+                    "Managing and configuring IP cameras, editing captured videos for presentations, and maintaining the security and integrity of video evidence.",
+                    "Providing technical support to students and staff, troubleshooting software-related issues, and offering guidance on using software applications effectively.",
+                    "Collaborating with cross-functional teams to enhance IT infrastructure, including transitioning from unmanaged to managed switches and optimizing access points and point-to-point radios.",
+                    "Assisting in the training of staff to effectively use the ERP software and troubleshoot any issues they encountered.",
+                    "Developing software solutions for clients, addressing their specific needs and contributing to the department's services."
+                ]
+            }
+        ]
+    }
+);
 
-  export const langs: Language[] = [
-    { name: 'English', description: '' },
-    { name: 'Swahili', description: '' },
-  
-  ];
+
+export const skills: Skill[] = [
+    {
+        title: "Django & Rest Framework",
+        level: 100,
+    },
+    {
+        title: "Kotlin & Jetpack Compose",
+        level: 100,
+    },
+    {
+        title: "Java & Java Fx",
+        level: 100,
+    },
+    {
+        title: "Kotlin Ktor & Spring Boot",
+        level: 100,
+    },
+
+    {
+        title: "Relational Databases",
+        level: 100,
+    },
+    {
+        title: "HTML5 & CSS",
+        level: 100,
+    },
+    {
+        title: "Vue Js",
+        level: 100,
+    },
+    {
+        title: "React ,Next Js && Express",
+        level: 100,
+    },
+    {
+        title: "Golang",
+        level: 60,
+    },
+    {
+        title: "Sketch & Adobe Express",
+        level: 100,
+    },
+];
+
+
+export const educations: Education[] = [
+    {
+        degree: "Bsc. Information Technology",
+        school: "Maseno University",
+        duration: "2020 - May 2024",
+    },
+    {
+        degree: "Kenya Certificate of Secondary Education",
+        school: "St Peter's Sang'alo High School",
+        duration: "2015 - 2019",
+    },
+];
+
+export const langs: Language[] = [
+    {name: 'English', description: ''},
+    {name: 'Swahili', description: ''},
+
+];
