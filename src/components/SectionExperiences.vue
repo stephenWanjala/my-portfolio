1da55f379ca219f65e3f02ea5a8636d0a8e22856
Wnajala Stephen <stephenwanjala145@gmail.com>
2024-09-20 16:21:16 +0300

    Remove  commented scc causing  an Error

--- src/components/SectionExperiences.vue
+++ src/components/SectionExperiences.vue
@@ -100,7 +100,6 @@ const formatTimeRange = (start: Date, end?: Date, current?: boolean): string => 
 
 <style scoped>
 .experience-section {
-  //font-family: -apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', 'Fira Sans', Ubuntu, Oxygen, 'Oxygen Sans', Cantarell, 'Droid Sans', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Lucida Grande', Helvetica, Arial, sans-serif;
   max-width: 800px;
   //margin: 0 auto;
   padding: 24px;
