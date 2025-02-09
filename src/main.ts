a89dc13265a11ab7a22942badfd825c92e8d9ef4
Wanjala Stephen <stephenwanjala145@gmail.com>
2025-02-09 16:34:34 +0300

    Updates

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
-// import 'bootstrap/dist/css/bootstrap.css'
-// Vuetify
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
